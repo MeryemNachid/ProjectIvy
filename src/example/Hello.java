5ffc4c0faf9e0529bb0905a448c69bf2d512dd4b
Nachid Meryem <mnachid@SA203-07A.ensi-bourges.fr>
2017-04-11 18:58:21 +0200

    Updated hello.class

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -1,14 +1,43 @@
 
 package example;
 
+import java.io.IOException;
+
 import org.apache.commons.lang.WordUtils;
 
 public class Hello {
-    public static void main(String[] args) {
-        String  message = "hello ivy !";
-        System.out.println("standard message : " + message);
-        System.out.println("capitalized by " + WordUtils.class.getName() 
+
+
+	public static void main(String[] args) {
+        	String  message = "hello ivy !";
+		System.out.println("standard message : " + message);
+		System.out.println("capitalized by " + WordUtils.class.getName() 
                                 + " : " + WordUtils.capitalizeFully(message));
-    }
+		int monmax = 0;
+		try {
+			CSVReader reader = new CSVReader(new FileReader("data.csv"));
+		
+			List<String[]> myEntries = reader.readAll();
+			while (reader.hasNext()){
+
+				int number = reader.next();
+				if (!number.equals("")){
+					int temp = Integer.parseInt(number);
+					System.out.println("Nombre lu : " +temp);
+
+					if(temp > monmax){
+						monmax = temp;
+					}
+				}
+			}
+			reader.close();			
+		} catch (IOException e){
+			System.out.println("Parse ERROR");
+		}
+		
+		System.out.println("Nombre maximum : " + monmax);
+
+	}
 }
         
+
